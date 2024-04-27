dc242ab62738085e770428e7dc2f762494f99df2
Giovanni Sabogal Cespedes <giovannisc22@gmail.com>
2024-04-28 00:00:59 +0200

    Implementacion Nav para movil

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+// import tailwindcssNesting from 'tailwindcss/nesting'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
+  // ,vite: {
+  //   css: {
+  //     postcss: {
+  //       plugins: [tailwindcssNesting()]
+  //     }
+  //   }
+  // }
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,37 @@
+import animations from 'tailwindcss-animated'
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				'yellow-ad': 		'#F8E082',
+				'primary-black-ad':	'#06050A',
+				'yellow-ad-sec': 	'#DBB84F',
+				'silver-ad': 		'#C1C0C5',
+				'silver-ad-logo':	'#D3D2D7',
+				'silver-ad-lines':	'#A4A3A8'
+
+			},
+			backgroundImage: {
+				'disenos-arquitectonicos': 	"url('/img/portada/CardServicios/Disenosarquitectonicos.webp')",
+				'disenos-interiores': 		"url('/img/portada/CardServicios/Disenos-de-interiores-y-cocinas.webp')",
+				'remodelaciones': 			"url('/img/portada/CardServicios/Remodelaciones.webp')",
+				'impermeabilizaciones': 	"url('/img/portada/CardServicios/Impermeabilizaciones.webp')",
+				'mantenimientos': 			"url('/img/portada/CardServicios/Mantenimientos.webp')",
+			},
+			boxShadow:{
+				'address': 'rgb(238, 245, 138) 3px 3px 6px 0px inset, rgba(255,255,255,0.2) -3px 3px 6px 1px inset'
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		// animations,
+		{
+			'postcss-import': {},
+			'tailwindcss/nesting': {},
+			tailwindcss: {},
+			autoprefixer: {},
+		}
+	],
 }
