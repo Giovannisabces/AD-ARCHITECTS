d0ab853a8e077bc4ecc84a62f0234fb66c3fcb33
Giovanni Sabogal Cespedes <giovannisc22@gmail.com>
2024-05-07 21:07:44 +0200

    delete boilerplate code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-// import tailwindcssNesting from 'tailwindcss/nesting'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-  // ,vite: {
-  //   css: {
-  //     postcss: {
-  //       plugins: [tailwindcssNesting()]
-  //     }
-  //   }
-  // }
+  integrations: [tailwind()],
+  output: 'hybrid'
 });
